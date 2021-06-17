cda0d8623f97c191a6c8f30747df84fb02bbc37c
b.benken <b.benken@famedly.com>
2021-06-17 13:41:13 +0200

    added missing override annotation

--- lib/src/UserAgentClient.dart
+++ lib/src/UserAgentClient.dart
@@ -6,6 +6,7 @@ class UserAgentClient extends BaseClient {
 
   UserAgentClient(this.userAgent, this._inner);
 
+  @override
   Future<StreamedResponse> send(BaseRequest request) {
     request.headers['user-agent'] = userAgent;
     return _inner.send(request);
